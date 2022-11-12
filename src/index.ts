926ca463c5aea4c257bf9d3541ba284e102d5ab2
ponciano.raf <ponciano.raf@nv99.com.br>
2022-11-11 23:18:11 -0300

    commit do receba

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.send('Express + TypeScript Server');
+  res.send('RECEBA');
 });
 
 app.listen(port, () => {
